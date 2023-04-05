64822ece0345a4454e579ec23b45b7613394e1b1
RamanAgrawal <iramanagrawal@gmail.com>
2023-04-06 00:30:00 +0530

    login added

--- Authentication/src/components/Auth/AuthForm.js
+++ Authentication/src/components/Auth/AuthForm.js
@@ -1,29 +1,62 @@
-import { useState } from 'react';
+import { useRef, useState } from 'react';
 
 import classes from './AuthForm.module.css';
+import axios from 'axios';
 
 const AuthForm = () => {
   const [isLogin, setIsLogin] = useState(true);
-
+  const [loading, setLoading] = useState(false)
+  const emailRef = useRef()
+  const passwordRef = useRef()
   const switchAuthModeHandler = () => {
     setIsLogin((prevState) => !prevState);
   };
+  const submitHandler = async (e) => {
+    e.preventDefault();
+    const enteredEmail = emailRef.current.value;
+    const enteredPassword = passwordRef.current.value;
+    setLoading(true)
+    let url;
+    if (isLogin) {
+      url = 'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyAyqFjwekzckK01VIQTo6f0bFFrPZrmDyI'
+    } else {
+      url = 'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyAyqFjwekzckK01VIQTo6f0bFFrPZrmDyI'
+    }
+     try {
+      const res = await axios.post(url, {
+        email: enteredEmail,
+        password: enteredPassword,
+        returnSecureToken: true
+      })
+      console.log(res.data.idToken);
+      alert('Success')
+    } catch (error) {
+      console.log();
+      alert('Authentication failed..!');
+    }
+    setLoading(false)
+
+
+
+
+    e.target.reset()
+  }
 
   return (
     <section className={classes.auth}>
       <h1>{isLogin ? 'Login' : 'Sign Up'}</h1>
-      <form>
+      <form onSubmit={submitHandler}>
         <div className={classes.control}>
           <label htmlFor='email'>Your Email</label>
-          <input type='email' id='email' required />
+          <input type='email' id='email' ref={emailRef} required />
         </div>
         <div className={classes.control}>
           <label htmlFor='password'>Your Password</label>
-          <input type='password' id='password' required />
+          <input type='password' id='password' ref={passwordRef} required />
         </div>
-        {!isLogin&&<p>sending request</p>}
         <div className={classes.actions}>
-          <button>{isLogin ? 'Login' : 'Create Account'}</button>
+          {!loading && <button>{isLogin ? 'Login' : 'Create Account'}</button>}
+          {loading && <p>sending request</p>}
           <button
             type='button'
             className={classes.toggle}
@@ -31,7 +64,7 @@ const AuthForm = () => {
           >
             {isLogin ? 'Create new account' : 'Login with existing account'}
           </button>
-         
+
         </div>
       </form>
     </section>
